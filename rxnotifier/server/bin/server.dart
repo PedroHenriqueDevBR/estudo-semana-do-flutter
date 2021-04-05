7f5d5a1cef1be432e127e957748607e502e89aa1
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-04-04 22:46:11 -0300

    feat: create server and docker image

--- rxnotifier/server/bin/server.dart
+++ rxnotifier/server/bin/server.dart
@@ -1,3 +1,49 @@
+import 'dart:io';
+
+import 'package:socket_io/socket_io.dart';
+import 'package:common/common.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  final Server server = Server();
+
+  server.on('connection', (client) {
+    onConnection(client);
+  });
+
+  server.listen(Platform.environment['PORT'] ?? 3000);
+}
+
+void onConnection(Socket socket) {
+  socket.on('enter_room', (data) {
+    final name = data['name'];
+    final room = data['room'];
+
+    socket.join(room);
+
+    socket.to(room).broadcast.emit(
+          'message',
+          SocketEvent(
+            name: name,
+            room: room,
+            text: '',
+            type: SocketEventType.enter_room,
+          ).toJson(),
+        );
+
+    socket.to(room).broadcast.emit(
+          'message',
+          SocketEvent(
+            name: name,
+            room: room,
+            text: '',
+            type: SocketEventType.leave_room,
+          ).toJson(),
+        );
+
+    socket.on('message', (json) {
+      socket.to(room).broadcast.emit('message', json);
+    });
+  });
+
+  socket.on('disconected', (data) {});
 }
